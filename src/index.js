8a72ac3996923b1113cd2f94e7c9612c0d1589da
Unknown <guillem.lion.benosa@gmail.com>
2018-06-08 12:11:29 +0200

    Switched to memory router to avoid broken links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { MemoryRouter as Router } from 'react-router-dom'
 import App from './App';
 import Header from './Header';
 import registerServiceWorker from './registerServiceWorker';
