d214a9443771eac69b0727aa72fa8ea0deb58893
Unknown <guillem.lion.benosa@gmail.com>
2018-06-08 12:11:11 +0200

    Added instructional videos in the Automatic setup

--- src/Instructions/AutomaticSetup.js
+++ src/Instructions/AutomaticSetup.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import DownloadMobileConfig from './DownloadMobileConfig'
 
+const youtubeLink = {
+  MacOS: 'https://www.youtube.com/embed/NTCjKue3ynM',
+  iOS: 'https://www.youtube.com/embed/_Dt_HmdynDU'
+}
+
+
 export default class AutomaticSetup extends Component {
   render() {
     return (
@@ -10,6 +16,20 @@ export default class AutomaticSetup extends Component {
         <DownloadMobileConfig />
         <hr className="my-4"/>
         <p>If a file does not start to download shortly, please use the manual guide below</p>
+
+        <div className="videoWrapper">
+          <iframe
+            id="ytplayer"
+            type="text/html"
+            width="640"
+            height="360"
+            frameBorder="0"
+            allowFullScreen
+            src={youtubeLink[this.props.platform]}
+            >
+          </iframe>
+        </div>
+
       </div>
     );
   }
