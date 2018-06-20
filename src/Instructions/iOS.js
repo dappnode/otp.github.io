ecbd9edf41baa4065cb4a075200ddd4441f79ecb
Unknown <guillem.lion.benosa@gmail.com>
2018-06-20 17:26:01 +0200

    Prettified iOS.js

--- src/Instructions/iOS.js
+++ src/Instructions/iOS.js
@@ -10,19 +10,38 @@ export default class iOS extends Component {
         <AutomaticSetup platform={"iOS"} />
 
         <h4>iOS setup guide</h4>
-        <hr className="my-4"/>
-        <ol className='ListInstructions'>
-          <li>Go to Settings -> General -> VPN.
-          </li><li>Tap <strong>Add VPN Configuration...</strong>.
-          </li><li>Tap <strong>Type</strong>. Select <strong>L2TP</strong> and go back.
-          </li><li>Tap <strong>Description</strong> and enter anything you like.
-          </li><li>Tap <strong>Server</strong> and enter <Credential tag={'server'}/>
-          </li><li>Tap <strong>Account</strong> and enter <Credential tag={'user'}/>
-          </li><li>Tap <strong>Password</strong> and enter <Credential tag={'pass'}/>
-          </li><li>Tap <strong>Secret</strong> and enter <Credential tag={'psk'}/>
-          </li><li>Make sure the <strong>Send All Traffic</strong> switch is OFF.
-          </li><li>Tap <strong>Done</strong>.
-          </li><li>Slide the <strong>VPN</strong> switch ON.
+        <hr className="my-4" />
+        <ol className="ListInstructions">
+          <li>Go to Settings -> General -> VPN.</li>
+          <li>
+            Tap <strong>Add VPN Configuration...</strong>.
+          </li>
+          <li>
+            Tap <strong>Type</strong>. Select <strong>L2TP</strong> and go back.
+          </li>
+          <li>
+            Tap <strong>Description</strong> and enter anything you like.
+          </li>
+          <li>
+            Tap <strong>Server</strong> and enter <Credential tag={"server"} />
+          </li>
+          <li>
+            Tap <strong>Account</strong> and enter <Credential tag={"user"} />
+          </li>
+          <li>
+            Tap <strong>Password</strong> and enter <Credential tag={"pass"} />
+          </li>
+          <li>
+            Tap <strong>Secret</strong> and enter <Credential tag={"psk"} />
+          </li>
+          <li>
+            Make sure the <strong>Send All Traffic</strong> switch is OFF.
+          </li>
+          <li>
+            Tap <strong>Done</strong>.
+          </li>
+          <li>
+            Slide the <strong>VPN</strong> switch ON.
           </li>
           <li>
             Tap <strong>Type</strong>. Select <strong>L2TP</strong> and go back.
